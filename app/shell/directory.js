e82a1b33364c8be28862758033b5421ae76626dd
bcchoi <zaxrok@gmail.com>
2016-09-16 02:09:25 +0900

    googlt tts

--- app/shell/directory.js
+++ app/shell/directory.js
@@ -5,6 +5,41 @@ const readChunk = require('read-chunk');
 const fileType = require('file-type');
 const _ = require('lodash');
 var fs = require('fs');
+var Speaker = require('speaker');
+var lame = require('lame');
+var request = require("request");
+var platform = process.platform;
+var isWin32 = (platform == "win32");
+var Promise = require('bluebird');
+var googleTTS = require('google-tts-api');
+var text = 'thanks a lot for the detailed writeup, I am sure people will find it useful';
+googleTTS(text, 'en', 1)
+	.then(function (url) {
+		return new Promise(function(resolve, reject) {
+			var r = request({uri:url});
+			var length = 5000;
+			var timer;
+			var decoder = new lame.Decoder();
+			var speaker = new Speaker();
+
+			r.on('complete', function(e) {
+				length = e.socket.bytesRead/2;
+				if(isWin32) {
+					timer = setTimeout(function() {
+						speaker.close();
+						resolve();
+					}, length);
+				}
+			});
+
+			speaker.on("close", function() {
+				decoder = null;
+				resolve();
+			});
+
+			r.pipe(decoder).pipe(speaker);
+		});
+	});
 
 ipcMain.on('folderContent', (event, path) => {
     path = _.isArray(path) && path.length > 0 ? path[0] : path;
